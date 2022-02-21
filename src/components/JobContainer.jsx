6c52735364a4ba72c17b6314b156dc770cf7831b
Darcio Junior <darciojunior@terra.com.br>
2022-02-21 19:22:15 -0300

    v1.0.1

--- src/components/JobContainer.jsx
+++ src/components/JobContainer.jsx
@@ -1,7 +1,31 @@
 import './JobContainer.css'
+import logoImg from '../images/photosnap.svg'
 
-export default function JobContainer() {
+export default function JobContainer(props) {
+    let data = props.data
+
+    function isNew() {
+        if (data.new) return <p>NEW!</p>
+    }
+
+    function isFeatured() {
+        if (data.featured) return <p>FEATURED</p>
+    }
+
+
     return <div className='jobContainer'>
-        <p>Lista de jobs</p>
+        {data.logo}
+        <img src={logoImg} alt="" className='logo'/>
+        <p>{data.company}</p>
+        {isNew()}
+        {isFeatured()}
+        <p>{data.position}</p>
+        <p>{data.postedAt}</p>
+        <p>{data.contract}</p>
+        <p>{data.location}</p>
+        <p>{data.role}</p>
+        <p>{data.level}</p>
+        <p>{data.languages}</p>
+        <p>{data.tools}</p>
     </div>
 }
